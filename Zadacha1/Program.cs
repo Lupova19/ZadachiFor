6632ae234a497ae574481878ca69c0f5af2b3d16
Lupova19 <nadiqlupova@gmail.com>
2023-11-16 20:03:46 +0200

    first commit

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Таблица с числото {i}");
+                for (int l = 1; l <= 10; l++)
+                {
+                    Console.Write($"|{i * l}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine($"Сумата на числата от 1 до 100 е: {sum}");
+        }
+    }
+}
